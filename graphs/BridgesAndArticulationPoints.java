3fae5885c7209a3c648a9e6b13f1b54c379c8cde
Sridhar Vinodarao <svinodarao@groupon.com>
2021-06-21 09:59:37 +0530

    Debugged Travelling Salesman Algo

---
+++ graphs/BridgesAndArticulationPoints.java
@@ -0,0 +1,26 @@
+package graphs;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import graphs.datastructure.Edge;
+
+public class BridgesAndArticulationPoints {
+    public static List<Edge> findBridges(List<Edge>[] edges, int nodeCount) {
+        boolean[] visited = new boolean[nodeCount];
+        int[] ids = new int[nodeCount];
+        int[] low = new int[nodeCount];
+
+        List<Edge> bridges = new ArrayList<>();
+
+        for (int i = 0; i < nodeCount; i++) {
+
+        }
+
+        return bridges;
+    }
+
+    private void dfs(){
+
+    }
+}
--- graphs/TravellingSalesmanProblem.java
+++ graphs/TravellingSalesmanProblem.java
@@ -8,19 +8,19 @@ import java.util.Set;
 public class TravellingSalesmanProblem {
     private int        size;
     private double[][] dist;
-    private boolean    solved;
-    private double     minDistance = Double.POSITIVE_INFINITY;
-    List<Integer> tour;
-    private int        start;
-    private double[][] memo;
-    private final int FINAL_STATE;
+    private boolean solved;
+    private double  tourCost = Double.POSITIVE_INFINITY;
+    List<Integer> tour = new ArrayList<>();
+    private       int        start;
+    private       double[][] costToGoTo;
+    private final int        WITH_ALL_NODES_IN_PATH;
 
     public TravellingSalesmanProblem(double[][] dist, int start) {
         this.dist = dist;
         this.start = start;
         this.size = dist.length;
-        this.memo = new double[size][(int) Math.pow(2, size)];
-        this.FINAL_STATE = (1 << size) -1;
+        this.costToGoTo = new double[size][(int) Math.pow(2, size)];
+        this.WITH_ALL_NODES_IN_PATH = (1 << size) - 1;
     }
 
     public List<Integer> getTour() {
@@ -33,86 +33,103 @@ public class TravellingSalesmanProblem {
     private void setup() {
         for (int i = 0; i < size; i++) {
             if (i == start) {continue;}
-            memo[i][1 << start | 1 << i] = dist[start][i];
+            costToGoTo[i][1 << start | 1 << i] = dist[start][i];
         }
     }
 
-    private Set<Integer> combinations(int sub_size){
+    private Set<Integer> combinations(int sub_size) {
         Set<Integer> allSets = new HashSet();
         combinations(allSets, sub_size, 0, 0);
         return allSets;
     }
 
-    private void combinations(Set<Integer> allSets, int length, int at, int bitInt){
-        if(length == 0){
-            allSets.add(bitInt);
+    private void combinations(Set<Integer> allSets, int setSize, int at, int currentSet) {
+        if (setSize == 0) {
+            allSets.add(currentSet);
             return;
         }
-        for(int i=at; i<size; i++){
-            bitInt |= (1 << i);
-            combinations(allSets, length-1, i+1, bitInt);
-            bitInt &= ~(1 << i);
+        for (int i = at; i < size; i++) {
+            currentSet |= (1 << i);
+            combinations(allSets, setSize - 1, i + 1, currentSet);
+            currentSet &= ~(1 << i);
         }
     }
 
-    private boolean notIn(int allBits, int bit){
-        return (allBits & bit) == 0;
+    private boolean notIn(int allBits, int bit) {
+        return (allBits & (1 << bit)) == 0;
     }
 
     private void solve() {
         setup();
         //capture min distance for all the path lengths & combinations.
-        for(int sub_length = 3; sub_length <= size; sub_length++){
-            for(int bitInt: combinations(sub_length)){
-                if(notIn(bitInt, start)) continue;
-                for(int current=0; current < size; current++){
-                    int previous_bits = bitInt & (~ (1 << current));
+        for (int sub_length = 3; sub_length <= size; sub_length++) {
+            for (int withNodesInPath : combinations(sub_length)) {
+                if (notIn(withNodesInPath, start)) { continue; }
+                for (int newNode = 0; newNode < size; newNode++) {
+                    if (newNode == start || notIn(withNodesInPath, newNode)) {continue;}
+                    int nodesSetWithoutNewNode = withNodesInPath ^ (1 << newNode);
                     double minDistance = Double.POSITIVE_INFINITY;
-                    for(int prev=0; prev < size; prev++){
-                        if(prev == start || prev == current || notIn(previous_bits, prev)){
+                    for (int toLastNode = 0; toLastNode < size; toLastNode++) {
+                        if (toLastNode == start || toLastNode == newNode || notIn(nodesSetWithoutNewNode, toLastNode)) {
                             continue;
                         }
-                        if(memo[prev][previous_bits] + memo[prev][current] < minDistance){
-                            minDistance = memo[prev][previous_bits] + memo[prev][current];
+                        double distanceFromTheLastNode = costToGoTo[toLastNode][nodesSetWithoutNewNode] + dist[toLastNode][newNode];
+                        if (distanceFromTheLastNode < minDistance) {
+                            minDistance = distanceFromTheLastNode;
                         }
                     }
-                    memo[current][bitInt] = minDistance;
+                    costToGoTo[newNode][withNodesInPath] = minDistance;
                 }
             }
         }
 
-        //get min cost for the shortest path
-        for(int i=0; i < size; i++){
-            if(i == start){
+        //get min cost across all the nodes with all nodes in path to go back to the start node
+        double costOfTheShortestPath = Double.POSITIVE_INFINITY;
+        for (int i = 0; i < size; i++) {
+            if (i == start) {
                 continue;
             }
-            if(memo[i][FINAL_STATE] + memo[i][start] < minDistance){
-                minDistance = memo[i][FINAL_STATE] + memo[i][start];
+            double totalCostToGoBackToStart = costToGoTo[i][WITH_ALL_NODES_IN_PATH] + dist[i][start];
+            if (totalCostToGoBackToStart < costOfTheShortestPath) {
+                costOfTheShortestPath = totalCostToGoBackToStart;
             }
         }
+        tourCost = costOfTheShortestPath;
 
         //get complete route for the shortest path
-        if(minDistance == Double.POSITIVE_INFINITY) {return;}
+        if (tourCost == Double.POSITIVE_INFINITY) {return;}
         solved = true;
-        List<Integer> path = new ArrayList();
-        path.add(start);
+        tour.add(start);
         int next = start;
-        int STATE = FINAL_STATE;
-        for(int i=1; i<size; i++){
+        int withNodesInPath = WITH_ALL_NODES_IN_PATH;
+        for (int i = 1; i < size; i++) {
             int min_at = -1;
-            for(int at=0; at < size; at++){
-                if(at == next || notIn(STATE, at)){continue;}
-                if(min_at == -1){min_at=at;}
-                if((memo[at][STATE] + memo[at][start]) < (memo[min_at][STATE])){
-                    min_at = at;
+            for (int node = 0; node < size; node++) {
+                if (node == start || notIn(withNodesInPath, node)) {continue;}
+                if (min_at == -1) {min_at = node;}
+                double currentMinCostToGoToNode = costToGoTo[min_at][withNodesInPath] + dist[min_at][next];
+                double newCostToGoToNode = costToGoTo[node][withNodesInPath] + dist[node][next];
+                if (newCostToGoToNode < currentMinCostToGoToNode) {
+                    min_at = node;
                 }
             }
             next = min_at;
-            STATE &= ~(1 << min_at);
-            path.add(min_at);
+            withNodesInPath ^= (1 << min_at);
+            tour.add(min_at);
         }
+        tour.add(start);
+    }
 
-        path.add(start);
+    public static void main(String[] args) {
+        double[][] graph = new double[][]{
+                {0.0, 10.0, 15.0, 20.0},
+                {10.0, 0.0, 35.0, 25.0},
+                {15.0, 35.0, 0.0, 30.0},
+                {20.0, 25.0, 30.0, 0.0}};
+        TravellingSalesmanProblem tsmp = new TravellingSalesmanProblem(graph, 0);
+        System.out.println(tsmp.getTour());
+        System.out.println(tsmp.tourCost);
+        //10 + 25 + 30 + 15 = 80
     }
 
 }
