7c9a660c8a2e7a0dfec68a3110d24b54fce52fd9
Sridhar Vinodarao <svinodarao@groupon.com>
2021-06-22 20:58:11 +0530

    bridges & articulation points

--- graphs/BridgesAndArticulationPoints.java
+++ graphs/BridgesAndArticulationPoints.java
@@ -2,7 +2,10 @@ package graphs;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
+import java.util.stream.IntStream;
 
 import graphs.datastructure.Edge;
 
@@ -12,12 +15,15 @@ public class BridgesAndArticulationPoints {
     boolean[]    visited;
     List<Edge>[] edges;
     List<Edge>   bridges = new ArrayList<>();
+    boolean[]    articulationPoints;
     int          nodeCount;
+    int          outgoingCount;
 
     public static BridgesAndArticulationPoints bootAlgorithm(List<Edge>[] edges) {
         BridgesAndArticulationPoints algo = new BridgesAndArticulationPoints();
         algo.nodeCount = edges.length;
         algo.visited = new boolean[algo.nodeCount];
+        algo.articulationPoints = new boolean[algo.nodeCount];
         algo.ids = new int[algo.nodeCount];
         algo.low = new int[algo.nodeCount];
         algo.edges = edges;
@@ -28,14 +34,14 @@ public class BridgesAndArticulationPoints {
 
         for (int i = 0; i < nodeCount; i++) {
             if (!visited[i]) {
-                dfs(i, -1);
+                dfsBridges(i, -1);
             }
         }
 
         return bridges;
     }
 
-    private void dfs(int node, int parent) {
+    private void dfsBridges(int node, int parent) {
         ids[node] = low[node] = sequence++;
         visited[node] = true;
 
@@ -45,15 +51,51 @@ public class BridgesAndArticulationPoints {
                 low[node] = ids[e.to];
                 continue;
             }
-            dfs(e.to, node);
+            dfsBridges(e.to, node);
             if (low[e.to] < low[node]) {
                 low[node] = low[e.to];
             }
-            if (low[node] < low[e.to]) {
+            if (ids[node] < low[e.to]) {
                 bridges.add(e);
             }
         }
+    }
 
+    private void dfsArticulationPoints(int root, int node, int parent) {
+        ids[node] = low[node] = sequence++;
+        if (parent == root) {
+            outgoingCount++;
+        }
+        visited[node] = true;
+
+        for (Edge e : edges[node]) {
+            if (e.to == parent) { continue; }
+            if (visited[e.to]) {
+                low[node] = ids[e.to];
+                continue;
+            }
+
+            dfsArticulationPoints(root, e.to, node);
+
+            if (low[e.to] < low[node]) {
+                low[node] = low[e.to];
+            }
+
+            if (ids[node] <= low[e.to]) {
+                articulationPoints[node] = true;
+            }
+        }
+
+    }
+
+    public boolean[] findArticulationPoints() {
+        for (int i = 0; i < nodeCount; i++) {
+            if (visited[i]) { continue; }
+            outgoingCount = sequence = 0;
+            dfsArticulationPoints(i, i, -1);
+            articulationPoints[i] = outgoingCount > 1;
+        }
+        return articulationPoints;
     }
 
     public static void main(String[] args) {
@@ -68,5 +110,7 @@ public class BridgesAndArticulationPoints {
         edges[7] = Arrays.asList(new Edge(7, 6), new Edge(7, 8));
         edges[8] = Arrays.asList(new Edge(8, 5), new Edge(8, 7));
         bootAlgorithm(edges).findBridges().stream().forEach(bridge -> System.out.println(bridge.from + " " + bridge.to));
+        boolean[] aps = bootAlgorithm(edges).findArticulationPoints();
+        IntStream.range(0, aps.length).forEach(i -> System.out.println(i + ":" + aps[i]));
     }
 }
